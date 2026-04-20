72ffb2e45bffe8f87a4f503f2ce7e5c416884965
Miles Davenport <milesd@chocksaway.com>
2026-04-20 19:16:42 +0100

    Added handle_restore and handle_copy.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 fn main() {
     const USAGE: &str = "Usage: gitcr --copy path_to_repo | --restore path_to_git_repo";
     // let args: Vec<String> = std::env::args().skip(1).collect();
-    let args = vec!["--copy".to_string(), "path".to_string()];
-
+    // let args = vec!["--copy".to_string(), "path".to_string()];
+    let args = vec!["--restore".to_string(), "path".to_string(), "repo".to_string()];
 
     if !has_two_or_three_args_and_correct_copy_or_restore(&args) {
         println!("Expected 2 or 3 command-line arguments.");
         println!("{USAGE}");
         return;
     }
+
+    match args[0].as_str() {
+        "--copy" => {
+            let path = &args[1];
+            handle_copy(path);
+        }
+        "--restore" => {
+            let git_repo = &args[1];
+            let target_path = &args[2];
+            handle_restore(git_repo, target_path);
+        }
+        _ => {
+            println!("Unknown command: {}", args[0]);
+            println!("{USAGE}");
+        }
+    }
+}
+
+fn handle_copy(path: &str) {
+    println!("Copying from: {}", path);
+    // TODO: Implement copy logic
+}
+
+fn handle_restore(git_repo: &str, target_path: &str) {
+    println!("Restoring from target path git repo: {} {}", git_repo, target_path);
+
+    // TODO: Implement restore logic
 }
 
 fn has_two_or_three_args_and_correct_copy_or_restore(args: &[String]) -> bool {
